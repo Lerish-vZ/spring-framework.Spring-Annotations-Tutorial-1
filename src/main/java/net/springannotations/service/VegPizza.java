96b155f71689413b657f53514216a35f9c40c282
Lerish-vZ <lerishavz@gmail.com>
2025-01-07 12:49:08 +0200

    update

--- src/main/java/net/springannotations/service/VegPizza.java
+++ src/main/java/net/springannotations/service/VegPizza.java
@@ -1,6 +1,7 @@
 package net.springannotations.service;
 
 import net.springannotations.controller.PizzaController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class VegPizza {
     private VegPizza vegPizza;
 
+    @Autowired //tells spring container to automatically inject the vegPizza (above) with a bean using the constructor below
     public PizzaController(VegPizza vegPizza){
         this.vegPizza = vegPizza;
     }
