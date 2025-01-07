e0b30cd56cceeb9cd4ad87d0666349ca838f67a7
Lerish-vZ <lerishavz@gmail.com>
2025-01-07 14:55:15 +0200

    update

--- src/main/java/net/springannotations/controller/PizzaController.java
+++ src/main/java/net/springannotations/controller/PizzaController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 //If you do not declare a bean name Spring automatically names the bean the class name with the first letter in lowercase
 //indicates that an annotated class is a "spring bean/component". Tells Spring container to automatically create Spring bean.
 public class PizzaController {
+    @Autowired //this is field injection
     private VegPizza vegPizza;
 
 //    @Autowired
@@ -15,11 +16,13 @@ public class PizzaController {
 //    public PizzaController(VegPizza vegPizza){
 //        this.vegPizza = vegPizza;
 //    }
+    //constructor injection
 
-    @Autowired //tells spring container to inject this spring bean as a dependency with the bottom setter method
-    public void setVegPizza(VegPizza vegPizza) {
-        this.vegPizza = vegPizza;
-    }
+//    @Autowired //tells spring container to inject this spring bean as a dependency with the bottom setter method
+//    public void setVegPizza(VegPizza vegPizza) {
+//        this.vegPizza = vegPizza;
+//    }
+    //setter injection
 
     public String getPizza(){
         return vegPizza.getPizza();
