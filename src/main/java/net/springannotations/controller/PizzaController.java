139ee6632daa64a4b4000f066c910bc5dc6c2c5c
Lerish-vZ <lerishavz@gmail.com>
2025-01-07 12:53:30 +0200

    update

--- src/main/java/net/springannotations/controller/PizzaController.java
+++ src/main/java/net/springannotations/controller/PizzaController.java
@@ -1,14 +1,22 @@
 package net.springannotations.controller;
 
+import net.springannotations.service.VegPizza;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component//("pizzaDemo")//"" is the spring name
 //If you do not declare a bean name Spring automatically names the bean the class name with the first letter in lowercase
 //indicates that an annotated class is a "spring bean/component". Tells Spring container to automatically create Spring bean.
 public class PizzaController {
+    private VegPizza vegPizza;
 
+    @Autowired
+    //tells spring container to automatically inject the vegPizza (above) with a bean using the constructor below
+    public PizzaController(VegPizza vegPizza){
+        this.vegPizza = vegPizza;
+    }
     public String getPizza(){
-        return "Hot Pizza";
+        return vegPizza.getPizza();
     }
 }
  
--- src/main/java/net/springannotations/service/VegPizza.java
+++ src/main/java/net/springannotations/service/VegPizza.java
@@ -7,15 +7,8 @@ import org.springframework.stereotype.Component;
 @Component
 
 public class VegPizza {
-    private VegPizza vegPizza;
-
-    @Autowired //tells spring container to automatically inject the vegPizza (above) with a bean using the constructor below
-    public PizzaController(VegPizza vegPizza){
-        this.vegPizza = vegPizza;
-    }
-
 
     public String getPizza(){
-        return vegPizza.getPizza();
+        return "Veg Pizza";
     }
 } 
