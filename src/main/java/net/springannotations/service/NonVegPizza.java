0ae6b01d948ac75254440d83b43fb7277ac52f23
Lerish-vZ <lerishavz@gmail.com>
2025-01-07 14:59:16 +0200

    update

--- src/main/java/net/springannotations/service/NonVegPizza.java
+++ src/main/java/net/springannotations/service/NonVegPizza.java
@@ -1,4 +1,12 @@
 package net.springannotations.service;
 
-public class NonVegPizza {
+import org.springframework.stereotype.Component;
+
+@Component
+public class NonVegPizza implements Pizza{
+
+    @Override
+    public String getPizza() {
+        return "Non-veg Pizza";
+    }
 }
