9fc206c228762b73170c84eb313fb0f996a640ac
Lerish-vZ <lerishavz@gmail.com>
2025-01-07 12:44:52 +0200

    update

--- src/main/java/net/springannotations/service/VegPizza.java
+++ src/main/java/net/springannotations/service/VegPizza.java
@@ -1,4 +1,12 @@
 package net.springannotations.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class VegPizza {
+    private
+    public String getPizza(){
+        return "Veg Pizza";
+    }
 } 
