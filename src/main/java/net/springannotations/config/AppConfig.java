cd9f5cc49743b1fa3ef94cd31a561c6784796b51
Lerish-vZ <lerishavz@gmail.com>
2025-01-07 15:32:46 +0200

    update

--- src/main/java/net/springannotations/config/AppConfig.java
+++ src/main/java/net/springannotations/config/AppConfig.java
@@ -1,9 +1,15 @@
 package net.springannotations.config;
 
+import net.springannotations.service.Pizza;
+import net.springannotations.service.VegPizza;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //makes this class a configuration class
 public class AppConfig { //within this configuration class we can define Spring bean definitions
 
-
+    @Bean //Tells spring container to manage this vegPizza class object
+    public Pizza vegPizza(){
+        return new VegPizza();
+    }
 }
