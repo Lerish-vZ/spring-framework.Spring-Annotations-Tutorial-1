c176c75c92fab9609b7f2228f61b456f502dfd88
Lerish-vZ <lerishavz@gmail.com>
2025-01-15 11:47:58 +0200

    update

--- src/main/java/net/springannotations/repository/MyRepository.java
+++ src/main/java/net/springannotations/repository/MyRepository.java
@@ -1,4 +1,11 @@
 package net.springannotations.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MyRepository {
+
+    public String hello(){
+        return "Hello Repository";
+    }
 }
