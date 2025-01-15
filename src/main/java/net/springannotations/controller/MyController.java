73ce4103fe1b0eb0b8f0aa56cadc33ccba9e60cd
Lerish-vZ <lerishavz@gmail.com>
2025-01-15 11:41:37 +0200

    update

--- src/main/java/net/springannotations/controller/MyController.java
+++ src/main/java/net/springannotations/controller/MyController.java
@@ -1,4 +1,11 @@
 package net.springannotations.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller//automatically create spring bean for class
 public class MyController {
+
+    public String hello(){
+        return "Hello Controller";
+    }
 }
