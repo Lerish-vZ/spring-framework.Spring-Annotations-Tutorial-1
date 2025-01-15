67dd058944f012badd697e15d3bc3694ff8bb23b
Lerish-vZ <lerishavz@gmail.com>
2025-01-15 11:43:40 +0200

    update

--- src/main/java/net/springannotations/SpringAnnotationsApplication.java
+++ src/main/java/net/springannotations/SpringAnnotationsApplication.java
@@ -1,5 +1,6 @@
 package net.springannotations;
 
+import net.springannotations.controller.MyController;
 import net.springannotations.controller.PizzaController;
 import net.springannotations.service.VegPizza;
 import org.springframework.boot.SpringApplication;
@@ -11,8 +12,11 @@ public class SpringAnnotationsApplication {
     public static void main(String[] args) {
 
         var context = SpringApplication.run(SpringAnnotationsApplication.class, args);
-       PizzaController pizzaController = (PizzaController) context.getBean("pizzaController");
-        System.out.println(pizzaController.getPizza());
+//       PizzaController pizzaController = (PizzaController) context.getBean("pizzaController");
+//        System.out.println(pizzaController.getPizza());
+
+      MyController myController = context.getBean(MyController.class);
+      System.out.println(myController.hello());
 
 //        VegPizza vegPizza = context.getBean(VegPizza.class); //can specify a bean name but default is the method name
 //        System.out.println(vegPizza.getPizza());
