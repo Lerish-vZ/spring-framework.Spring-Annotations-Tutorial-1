ce4f2dafbf5950b4cd0abc82686532e120fd6eda
Lerish-vZ <lerishavz@gmail.com>
2025-01-15 11:46:48 +0200

    update

--- src/main/java/net/springannotations/SpringAnnotationsApplication.java
+++ src/main/java/net/springannotations/SpringAnnotationsApplication.java
@@ -2,6 +2,7 @@ package net.springannotations;
 
 import net.springannotations.controller.MyController;
 import net.springannotations.controller.PizzaController;
+import net.springannotations.service.MyService;
 import net.springannotations.service.VegPizza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +18,9 @@ public class SpringAnnotationsApplication {
 
       MyController myController = context.getBean(MyController.class);
       System.out.println(myController.hello());
+
+      MyService myService = context.getBean(MyService.class);
+      System.out.println(myService.hello());
 
 //        VegPizza vegPizza = context.getBean(VegPizza.class); //can specify a bean name but default is the method name
 //        System.out.println(vegPizza.getPizza());
