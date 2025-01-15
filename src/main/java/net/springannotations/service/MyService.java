6b35640aee38aceac3775f74a33074867c87d31e
Lerish-vZ <lerishavz@gmail.com>
2025-01-15 11:46:17 +0200

    update

--- src/main/java/net/springannotations/service/MyService.java
+++ src/main/java/net/springannotations/service/MyService.java
@@ -1,4 +1,11 @@
 package net.springannotations.service;
 
+import org.springframework.stereotype.Service;
+
+@Service //is specialized annotation of @Component annotation
 public class MyService {
+
+    public String hello(){
+        return "Hello Service";
+    }
 }
