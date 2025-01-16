c00477e2ddba6e7dfcdeb7c8ae3c08f21e6f4020
Lerish-vZ <lerishavz@gmail.com>
2025-01-16 12:31:32 +0200

    update

--- src/main/java/net/springannotations/configurationproperties/AppProperties.java
+++ src/main/java/net/springannotations/configurationproperties/AppProperties.java
@@ -18,6 +18,38 @@ public class AppProperties {
         private String password;
         List<String> roles = new ArrayList<>();
         private boolean enabled;
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public List<String> getRoles() {
+            return roles;
+        }
+
+        public void setRoles(List<String> roles) {
+            this.roles = roles;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
     }
 
     public String getName() {
