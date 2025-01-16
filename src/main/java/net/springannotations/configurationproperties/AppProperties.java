1623a8376b21adf69886757cadcb2c474d6bd36b
Lerish-vZ <lerishavz@gmail.com>
2025-01-16 11:54:23 +0200

    update

--- src/main/java/net/springannotations/configurationproperties/AppProperties.java
+++ src/main/java/net/springannotations/configurationproperties/AppProperties.java
@@ -1,7 +1,24 @@
 package net.springannotations.configurationproperties;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "app")
+@Component
 public class AppProperties {
     private String name;
     private String description;
     private String uploadDir;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
 }
