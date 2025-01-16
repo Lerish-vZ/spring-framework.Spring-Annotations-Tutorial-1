8b3ad711e688bb8cb98ba0ba9284cf5b77d4e194
Lerish-vZ <lerishavz@gmail.com>
2025-01-16 13:18:17 +0200

    update

--- src/main/java/net/springannotations/controller/BookController.java
+++ src/main/java/net/springannotations/controller/BookController.java
@@ -1,7 +1,13 @@
 package net.springannotations.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller//becomes a Spring MVC Controller
 public class BookController {
+
+    @RequestMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
 }
