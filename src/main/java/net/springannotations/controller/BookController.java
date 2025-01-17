ea7dd63d59e870817a44cbfe65c4cea4cd65c3e1
Lerish-vZ <lerishavz@gmail.com>
2025-01-17 13:36:19 +0200

    update

--- src/main/java/net/springannotations/controller/BookController.java
+++ src/main/java/net/springannotations/controller/BookController.java
@@ -3,10 +3,7 @@ package net.springannotations.controller;
 import net.springannotations.beans.Book;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //@Controller//becomes a Spring MVC Controller
 //@ResponseBody
@@ -22,11 +19,12 @@ public class BookController {
     } //Handler methods
     //Above is how to use @RequestMapping for methods
 
-    @RequestMapping(value = {"/book", "/core-java", "java"}, //multiple URI
-    method = RequestMethod.GET, //HTTP Methods
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, //Tells Rest API What type it has to return in this case Json
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
-    )
+
+     @GetMapping(value = {"/book"}) 
+//     // @RequestMapping(value = {"/book", "/core-java", "java"},//multiple URI
+//     method = RequestMethod.GET//, //HTTP Methods
+//            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, //Tells Rest API What type it has to return in this case Json
+//            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     //@ResponseBody
     public Book getBook(){
         Book book = new Book(1, "Core Java", "Learn Core Java and Latest Features.");
