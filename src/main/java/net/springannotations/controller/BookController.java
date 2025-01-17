cc12044f0eeb9f2c1db3a64bdaaf2172faacfafa
Lerish-vZ <lerishavz@gmail.com>
2025-01-17 12:44:30 +0200

    update

--- src/main/java/net/springannotations/controller/BookController.java
+++ src/main/java/net/springannotations/controller/BookController.java
@@ -4,18 +4,21 @@ import net.springannotations.beans.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller//becomes a Spring MVC Controller
+//@Controller//becomes a Spring MVC Controller
+//@ResponseBody
+@RestController //combination of @Controller & @ResponseBody
 public class BookController {
 
     @RequestMapping("/hello-world") //Map incoming http request to this particular method
-    @ResponseBody //return type in Json format
+    //@ResponseBody //return type in Json format (@Controller & @ResponseBody)
     public String helloWorld(){
         return "Hello World";
     } //Handler methods
 
     @RequestMapping("/book")
-    @ResponseBody
+    //@ResponseBody
     public Book getBook(){
         Book book = new Book(1, "Core Java", "Learn Core Java and Latest Features.");
         return book;
